6175d7fba96bbac7242555ef507a8049d258b96f
yuxing26liu <165765356+yuxing26liu@users.noreply.github.com>
2024-05-07 17:04:47 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -22,26 +22,25 @@ class ListExamples {
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
  public static List<String> merge(List<String> list1, List<String> list2) {
-        List<String> result = new ArrayList<>();
-        int index1 = 0, index2 = 0;
-        while (index1 < list1.size() && index2 < list2.size()) {
-            if (list1.get(index1).compareTo(list2.get(index2)) < 0) {
-                result.add(list1.get(index1));
-                index1++;
-            } else {
-                result.add(list2.get(index2));
-                index2++;
-            }
-        }
-        while (index1 < list1.size()) {
-            result.add(list1.get(index1));
-            index1++;
-        }
-        while (index2 < list2.size()) {
-            result.add(list2.get(index2));
-            index2++;
-        }
-        return result;
+    List<String> result = new ArrayList<>();
+    int index1 = 0, index2 = 0;
+    while (index1 < list1.size() && index2 < list2.size()) {
+      if (list1.get(index1).compareTo(list2.get(index2)) < 0) {
+        result.add(list1.get(index1));
+        index1++;
+      } else {
+        result.add(list2.get(index2));
+        index2++;
+      }
     }
-
+    while (index1 < list1.size()) {
+      result.add(list1.get(index1));
+      index1++;
+    }
+    while (index2 < list2.size()) {
+      result.add(list2.get(index2));
+      index2++;
+    }
+    return result;
+  }
 }
